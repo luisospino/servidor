dae282db05b224cc42c7ace99d69392441e8b304
luisospino <luisospinoa@gmail.com>
2021-08-19 13:51:22 -0500

    Conexión a base de datos influx con proxy

--- app/proxy.py
+++ app/proxy.py
@@ -1,14 +1,26 @@
 # example_consumer.py
 import pika, os, csv
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
+from datetime import datetime
 
+my_bucket = "device1"
+my_org    = "taller3"
+my_token  = "oT3IoJqa9dllthFQ4ByP5yRlqz53jtLkSszbfpaseWxxm_06jic1RMGOVnZyxUu1dMmjIareFBx1kT-kKAmhbg=="
+
+client = InfluxDBClient(url="http://influxdb:8086", token=my_token, org=my_org)
 
 def process_function(msg):
   mesage = msg.decode("utf-8")
   print(mesage)
-  #Aqui va el codigo para enviar a la base de datos.
-  #
-  #
-  #
+      
+  write_api = client.write_api(write_options=SYNCHRONOUS)
+  query_api = client.query_api()
+
+  point = Point("Medicion").tag("Ubicacion", "Santa Marta").field("temperatura", float(mesage)).time(datetime.utcnow(), WritePrecision.NS)
+
+  write_api.write(my_bucket, my_org, point)
+    
   return
 
 while 1:
